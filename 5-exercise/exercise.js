bd5ac220674d7d7deabf7e47b765c7a76c8ea3a5
pablosantamaria <161372585+saintmaryIX@users.noreply.github.com>
2026-02-22 16:58:28 +0100

    v2

--- 5-exercise/exercise.js
+++ 5-exercise/exercise.js
@@ -10,24 +10,20 @@ fetch('https://jsonplaceholder.typicode.com/users/')
   .then(users => {
     console.log("--- Processed Users ---");
 
-    // 1) Filter: solo IDs pares
     const evenIdUsers = users.filter(user => user.id % 2 === 0); //me quedo con users de idpar
 
-    // 2) Transform: { id, name, city } sacando city de address (destructuring)
     const cleanedUsers = evenIdUsers.map(user => {
       const { id, name, address: { city } } = user; // creo cleaned users y hago q solo pillen los atributos id, name y city de eventidusers (q tenía muchos atributos)
       return { id, name, city };
     });
 
-    // 3) Add: insertar "Guest User" al inicio SIN mutar el array
     const guestUser = { id: 0, name: "Guest User", city: "N/A" };
     const finalUsers = [guestUser, ...cleanedUsers]; //hago spread y meto a la izq del vector de users un nuevouser "guestuser"
 
     console.log(finalUsers);
 
     console.log("--- Statistics ---");
-    // 4) Reduce: total de caracteres en TODOS los usernames combinados
-    // (usamos el array original `users`, porque la estadística pide "usernames")
+    
     const totalUsernameChars = users.reduce((acc, user) => {
       return acc + user.username.length;
     }, 0);
